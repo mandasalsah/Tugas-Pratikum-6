7c734d699940a8c645fb37d720f00654ce6ddf1e
mandasalsah <100126407+mandasalsah@users.noreply.github.com>
2022-04-27 20:57:17 +0700

    Update login.php

--- login.php
+++ login.php
@@ -4,8 +4,6 @@
  <meta charset="utf-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
  <link rel="stylesheet" type="text/css" href="style.css">
 
  <title>Form Login Manda Cantik</title>
@@ -15,19 +13,18 @@
   <form action="output.php" method="POST" class="login-email">
    <p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
    <div class="input-group">
-    <input type="nama" placeholder="Nama" name="nama" 
+    <input type="text" placeholder="Nama" name="nama" 
     <br>
     </br>
    </div>
    <div class="input-group">
-    <input type="email" placeholder="email" name="email" 
+    <input type="text" placeholder="email" name="email" 
     <br></br>
    </div>
    <div class="input-group">
     <button name="submit" class="btn">Login</button>
    </div>
-   <div class="input-group">
-    <button name="submit" class="btn">reset</button>
+   <input type="reset" name="reset" value="Hapus"></td>
    </div>
   </form>
  </div>
